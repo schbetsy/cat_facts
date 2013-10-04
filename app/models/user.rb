3eb210a6e843ac7d06962b9232cf017676569324
Betsy Lorton <elorton@gmail.com>
2013-10-04 14:40:34 -0500

    fix user.welcome

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
 
 	def welcome
-		send_fact(CatFacts.intro)
+		send_fact([CatFacts.intro])
 	end
   
   def send_fact(msg)
-
     sender = Twilio::REST::Client.new(ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"])
     msg.each do |m|
     	sender.account.messages.create( to: self.phone_number,
